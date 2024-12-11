2b5ccec46262ac5b46b3318f9c6fa73c3ae5045e
Mohammad Emami <mhmmd.emmi@gmail.com>
2024-12-11 23:14:16 +0330

    create: blog component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Blog from "./components/Blog";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <main className="overflow-x-hidden antialiased text-neutral-800">
         <Navbar />
         <Hero />
+        <Blog />
       </main>
     </>
   );
