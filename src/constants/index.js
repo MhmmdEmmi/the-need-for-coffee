5c80ad11fccad58557160a62236edcb827b94f4c
Mohammad Emami <mhmmd.emmi@gmail.com>
2024-12-11 19:01:35 +0330

    create: constants and some images

---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+import TheCoffeeCollective03 from "../assets/images/TheCoffeeCollective03.webp";
+
+export const LINKS = [
+  {
+    name: "خانه",
+    link: "#home",
+  },
+  {
+    name: "وبلاگ",
+    link: "#TheBlog",
+  },
+  {
+    name: "رویدادها",
+    link: "#events",
+  },
+  {
+    name: "داستان قهوه",
+    link: "#TheCoffeeStory",
+  },
+  {
+    name: "درباره من",
+    link: "#AboutMe",
+  },
+];
