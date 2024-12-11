2cced1eabb42655837bad76ebf25d6655a4ecfcf
Mohammad Emami <mhmmd.emmi@gmail.com>
2024-12-11 16:47:42 +0330

    test: tailwind configure property

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-const App = () => {
-  return <div>App</div>;
-};
-
-export default App;
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
