cbf2b2c04d8c81c0fc6f2a679c759f32adb51b09
Mohammad Emami <mhmmd.emmi@gmail.com>
2024-12-11 19:18:44 +0330

    add: logo section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,17 @@
 const Navbar = () => {
-  return <div>نوار ناوبری</div>;
+  return (
+    <>
+      <nav className="border-b-2">
+        <div className="max-w-7xl mx-auto flex justify-between items-center py-8">
+          <div className="pl-2">
+            <a href="#">
+              <img src={logo} alt="logo" width={150} height={15} />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
