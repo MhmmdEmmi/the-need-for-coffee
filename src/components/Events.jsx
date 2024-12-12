b017b18049fbc0b345e8efc51c2eacdd3058b26c
Mohammad Emami <mhmmd.emmi@gmail.com>
2024-12-12 19:18:57 +0330

    update: complete events section

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,5 +1,35 @@
+import { EVENTS_PROJECTS } from "../constants";
+
 const Events = () => {
-  return <>Events</>;
+  return (
+    <>
+      <section className="max-w-7xl mx-auto border-b-2" id="events">
+        <div className="mb-20">
+          <h2 className="text-xl lg:text-3xl tracking-tight text-center mt-20 mb-12">
+            رویدادهـا
+          </h2>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {EVENTS_PROJECTS.map((event) => (
+              <div
+                key={event.id}
+                className="group relative overflow-hidden rounded-3xl mx-4"
+              >
+                <img
+                  src={event.image}
+                  alt={event.name}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+                <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100">
+                  <h3 className="mb-2 text-xl font-medium">{event.name}</h3>
+                  <p className="mb-12 p-4">{event.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Events;
